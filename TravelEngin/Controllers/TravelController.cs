b4a34aa4790383a10dd25a9de5e705c9247c0b7b
siavsh <Ali.besharati@hotmail.com>
2024-09-29 13:08:42 +0330

    Add travel Contoller

--- TravelEngin/Controllers/TravelController.cs
+++ TravelEngin/Controllers/TravelController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using TravelEngin.DAL;
+using TravelEngin.DAL.Models;
 using TravelEngin.ViewModels;
 
 namespace TravelEngin.Controllers;
@@ -8,44 +10,97 @@ public class TravelController : Controller
 {
     private readonly ILogger<TravelController> _logger;
 
-    public TravelController(ILogger<TravelController> logger)
+    private readonly TravelEnginDataContext _context;
+
+    public TravelController(ILogger<TravelController> logger, TravelEnginDataContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
+        //TODO : show agency dashboard 
         return View();
     }
 
 
-    public IActionResult AboutUs()
+    // GET: TravelPackages/Create
+    public IActionResult CreatePackage()
     {
+        // Display the form for creating a new travel package
         return View();
     }
 
-    public IActionResult ContactUs()
+    // POST: TravelPackages/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult CreatePackage([Bind("Name,Description,Price,Duration")] TravelPackage travelPackage)
     {
-        return View();
+        if (ModelState.IsValid)
+        {
+            // Add the travel package to the database
+            travelPackage.CreatedDate = DateTime.Now;
+            travelPackage.UpdatedDate = DateTime.Now;
+            _context.Add(travelPackage);
+            _context.SaveChanges();
+
+            // Redirect to the list of travel packages or details view
+            return RedirectToAction(nameof(Index));
+        }
+
+        // If the model state is invalid, return the view with validation errors
+        return View(travelPackage);
     }
 
-    public IActionResult KnowledgeBase()
+    public IActionResult AddItinerary(int travelPackageId)
     {
-        return View();
+        // Find the travel package to ensure it exists
+        var travelPackage = _context.TravelPackages.Find(travelPackageId);
+        if (travelPackage == null)
+        {
+            return NotFound();
+        }
+
+        // Create a new itinerary object and set the TravelPackageId
+        var itinerary = new Itinerary
+        {
+            TravelPackageId = travelPackageId,
+            DayNumber = 1 // Default to Day 1, can be changed in the form
+        };
+
+        ViewBag.TravelPackageName = travelPackage.Name;
+        return View(itinerary);
     }
 
-    public IActionResult Terms()
+    // POST: Itineraries/AddItinerary
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult AddItinerary([Bind("TravelPackageId,DayNumber,Title,Description,Activities,Accommodation,Meals")] Itinerary itinerary)
     {
-        return View();
+        if (ModelState.IsValid)
+        {
+            // Add the itinerary to the database
+            itinerary.CreatedDate = DateTime.Now;
+            itinerary.UpdatedDate = DateTime.Now;
+            _context.Itineraries.Add(itinerary);
+            _context.SaveChanges();
+
+            // Redirect to the details of the travel package the itinerary belongs to
+            return RedirectToAction("Details", "TravelPackages", new { id = itinerary.TravelPackageId });
+        }
+
+        // If the model state is invalid, return the view with validation errors
+        return View(itinerary);
     }
 
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
 
-   
+
+
+
+
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
